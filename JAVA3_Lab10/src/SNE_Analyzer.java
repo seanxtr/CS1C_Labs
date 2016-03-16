2a883e39237743e30f2199c2360391f80d480f40
Student <Student@153.18.37.73>
2016-03-15 20:05:40 -0700

    add sth

---
+++ JAVA3_Lab10/src/SNE_Analyzer.java
@@ -0,0 +1,16 @@
+import cs_1c.*;
+class SNE_Analyzer extends  StarNearEarth
+{
+   private double x, y, z;
+   
+   // constructors
+   public SNE_Analyzer() { /* TBD */ }
+   public SNE_Analyzer( StarNearEarth sne ) { /* TBD */ }
+   
+   // accessors
+   double getX() { return x; }
+   double getY() { return y; }
+   double getZ() { return z; } 
+   public void calcCartCoords() { /* TBD */ }
+   public String coordToString() { /* TBD */ }
+}
